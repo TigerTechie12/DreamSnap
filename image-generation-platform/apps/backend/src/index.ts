172be45902068e82527fc3fb57f8d04599d75dfd
Shreyash <shreyashiitr22@gmail.com>
2025-07-26 23:12:02 +0530

    db migration

--- image-generation-platform/apps/backend/src/index.ts
+++ image-generation-platform/apps/backend/src/index.ts
@@ -1,10 +1,27 @@
 import express from 'express'
 import { TrainModel, GenerateImage, GenerateImagesFromPack } from '@shreyash_iitr/common';
+import { prisma } from 'db';
 const PORT=process.env.PORT || 3000
 
 const app=express()
+app.use(express.json())
+app.post('/ai/training',async(res:any,req:any)=>{
+    const parsedBody=TrainModel.safeParse(req.body)
+    if(!parsedBody){
+        return res.status(411)
+    }
+    await prisma.model.create({
+        data:{
+            name:parsedBody.data.name,
+            age:parsedBody.data.age,
+            eyeColor:parsedBody.data.eyeColor,
+            type:parsedBody.data.type,
+           ethinicity: parsedBody.data.ethinicity,
+            bald:parsedBody.data.bald
+        }
+    })
 
-app.post('/ai/training',(req,res)=>{})
+})
 
 app.post('/ai/generate',(req,res)=>{})
 
--- image-generation-platform/packages/common/src/types.ts
+++ image-generation-platform/packages/common/src/types.ts
@@ -2,11 +2,11 @@ import {z} from 'zod'
 
 export const TrainModel=z.object({
 name:z.string(),
-type:z.enum(["Man","Woman","Other"]),
+type:z.enum(["Man","Woman","Others",]),
 age:z.number(),
 ethinicity:z.enum(["White","Black",
-    "Asian American", "East Asian", "South East Asian",
-    "South Asian", "Middle Eastern", "Pacific", "Hispanic"
+    "Asian_American", "East_Asian", "South_East_Asian",
+    "South_Asian", "Middle_Eastern", "Pacific", "Hispanic"
 ]),
 eyeColor:z.enum(["Brown","Blue","Hazel","Gray"]),
 bald:z.boolean(),
