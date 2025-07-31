e737d94982d58f83796037493f903b5c09a071c7
Shreyash <shreyashiitr22@gmail.com>
2025-07-31 09:18:13 +0530

    db migrated and end points updated

--- image-generation-platform/apps/backend/src/index.ts
+++ image-generation-platform/apps/backend/src/index.ts
@@ -5,27 +5,55 @@ const PORT=process.env.PORT || 3000
 
 const app=express()
 app.use(express.json())
+const USER_ID='1123'
 app.post('/ai/training',async(res:any,req:any)=>{
     const parsedBody=TrainModel.safeParse(req.body)
     if(!parsedBody){
         return res.status(411)
     }
-    await prisma.model.create({
+const data=await prisma.model.create({
         data:{
             name:parsedBody.data.name,
             age:parsedBody.data.age,
             eyeColor:parsedBody.data.eyeColor,
             type:parsedBody.data.type,
            ethinicity: parsedBody.data.ethinicity,
-            bald:parsedBody.data.bald
+            bald:parsedBody.data.bald,
+            userId:USER_ID
         }
+    })
+    res.json({
+        modelId:data.id
     })
 
 })
 
-app.post('/ai/generate',(req,res)=>{})
+app.post('/ai/generate', async(req,res)=>{
+ const parsedBody=GenerateImage.safeParse(req.body)
+ if(!parsedBody){{
+ return res.status(411)
+ }}
+ const data=await prisma.OutputImages.create({
+    data:{
+        prompt:parsedBody.data.prompt,
+        modelId:parsedBody.data.modelId,
+userId:USER_ID,
+imageUrl:""
+    }
+})
 
-app.post('/pack/generate',(req,res)=>{})
+})
+
+app.post('/pack/generate',async(req,res)=>{
+    const parsedBody=GenerateImagesFromPack.safeParse(req.body)
+    if(!parsedBody){
+        return res.status(411)
+    }
+
+
+   })
+
+
 
 app.get('/pack/bulk',(req,res)=>{})
 
