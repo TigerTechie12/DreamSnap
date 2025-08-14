ad6b29d8154fb17bf7688274e9b9f4749e9a6912
Shreyash <shreyashiitr22@gmail.com>
2025-08-14 10:34:08 +0530

    Clean repository for new Turborepo setup

--- image-generation-platform/packages/db/prisma/migrations/20250805030047_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "ModelTrainingStatusEnum" AS ENUM ('Pending', 'generated', 'Failed');
-
--- AlterTable
-ALTER TABLE "Model" ADD COLUMN     "tensorPath" TEXT,
-ADD COLUMN     "trainingStatus" "ModelTrainingStatusEnum" NOT NULL DEFAULT 'Pending',
-ADD COLUMN     "triggerWord" TEXT;
