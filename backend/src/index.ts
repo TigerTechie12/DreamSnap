2f672cd3320e9eb9adc191819f28f2fd984d3518
Shreyash <shreyashiitr22@gmail.com>
2025-11-27 17:16:22 +0530

    db migrated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ const app=express()
 app.use(express.json())
 app.post('/ai/training',async(req,res)=>{
 const input=req.body
+const userId="test-user-id"
 const parsedResult=TrainModel.safeParse(input)
 if(!parsedResult.success){
 return res.status(400).json({message:"Invalid input"})
@@ -20,7 +21,7 @@ const dbData=await prismaClient.model.create({
         ethinicity:parsedResult.data.ethinicity,
         eyecolor:parsedResult.data.eye_color,
         bald:parsedResult.data.bald,
-       
+        userId:userId
     }
 
 })
@@ -28,9 +29,47 @@ return res.status(200).json({modelId:dbData.id})})
 
 
 
-app.post('/ai/generate',(req,res)=>{})
-app.post('/ai/pack/generate',(req,res)=>{})
-app.get('/pack/bulk',(req,res)=>{})
+app.post('/ai/generate',async(req,res)=>{
+    const generationBody=req.body
+    const parsedResult=GenerateImage.safeParse(generationBody)
+    if(!parsedResult.success){
+        return res.status(400).json({message:"Invalid input"})
+    }
+const dbData=await prismaClient.outputImages.create({
+    data:{
+            prompt:parsedResult.data.prompt,
+            modelId:parsedResult.data.modelId,
+            imageUrl:"",
+
+    }
+})
+return res.status(200).json({ImageId:dbData.id})
+})
+app.post('/ai/pack/generate',async(req,res)=>{
+    const packBody=req.body
+    const parsedResult=GenerateImagesFromPack.safeParse(packBody)
+    if(!parsedResult.success){
+        return res.status(400).json({message:"Invalid input"})
+    }
+const prompts=await prismaClient.packPrompts.findMany({
+where:{
+packId:parsedResult.data.packId
+}
+})
+const images=await prismaClient.outputImages.createMany({
+    data:prompts.map((prompt)=>({
+        prompt:prompt.prompt,
+        imageUrl:"",
+        modelId:parsedResult.data.modelId
+    }) )
+})
+return res.status(200).json({})
+})
+
+
+app.get('/pack/bulk',(req,res)=>{
+
+})
 app.get('/image',(req,res)=>{})
 
 app.listen(PORT,()=>{
