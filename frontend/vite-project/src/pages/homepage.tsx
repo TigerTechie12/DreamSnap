5a45fcfe66e2624eba52c080fd6c99bf7eea09bd
Shreyash <shreyashiitr22@gmail.com>
2025-12-07 23:03:37 +0530

    basic structure of homepage created

---
+++ frontend/vite-project/src/pages/homepage.tsx
@@ -0,0 +1,48 @@
+import { Navbar } from "../components/navbar"
+export function Homepage(){
+    return <div>
+<Navbar></Navbar>
+<div>
+    Create stunning AI photos of yourself
+</div>
+<div>Train a custom AI model with your photos and geenrate unlimited professional images. Perfect for social media, dating profiles, or just having fun.</div>
+<div className="flex">
+    <button>Start Creating Free </button>
+<button className="hover:bg-purple-500">Browse Packs</button>
+</div>
+<div>
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+</div>
+<div>Why choose DreamSnap?</div>
+<div>Our cutting-edge AI technology delivers the most realistic and personalized results.</div>
+   <div className="flex">
+    <span className="border-2 rounded-lg">
+        <div className="flex flex-col">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+</svg>
+<div className="font-white font-lg font-bold">Ultra Realistic</div>
+<div className="font-grey">State-of-the-art models for photorealisitic results</div>
+        </div>
+    </span>
+    <span className="border-2 rounded-lg"> <div className="flex flex-col">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+</svg>
+    <div className="font-white font-lg font-bold">Personalized Models</div>
+    <div className="font-grey">Train custom models using your own photos</div>
+        </div></span>
+    <span className="border-2 rounded-lg"> <div className="flex flex-col">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"></path><path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"></path><path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"></path><path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"></path></svg>
+        <div>Lightning Fast</div>
+        <div>Generate images in seconds with our optimized AI models</div>
+        </div></span>
+   </div>
+
+    </div>
+}
