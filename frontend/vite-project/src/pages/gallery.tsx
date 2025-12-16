c992570707c6a41dbfd54d669856882cc90f1884
Shreyash <shreyashiitr22@gmail.com>
2025-12-16 14:23:59 +0530

    updated pages for making API calls

--- frontend/vite-project/src/pages/gallery.tsx
+++ frontend/vite-project/src/pages/gallery.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
-import axios from axios
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+interface Photo{
+id:string
+modelId:string
+createdAt:string
+updatedAt:string
+prompt:string
+imageUrl:string
+status:string
+}
 export function Gallery(){
-    const [numberOfImages,setNumberOfImages]=useState()
-    const [photos,setPhotos]=useState([])
+    const [photos,setPhotos]=useState<Photo[]>([])
+
+    const navigate=useNavigate()
+  useEffect(()=>{
+    const fetchPhotos=async()=>{
+       const ph:any= await axios.get('')
+    setPhotos(ph)
+    }
+  },[photos])
+    
 return <div>
  <div className='flex'>
  <h1>My Gallery</h1>
@@ -11,10 +29,20 @@ return <div>
 
  </div>
    
-    <h4>{numberOfImages} images generated</h4>
-<input type="text" placeholder='search bar' />
+    <h4>{photos.length} images generated</h4>
+
 <div>
-    
+{photos.map((p)=>(
+p.status==='COMPLETED' ? <div className='border-b-amber-200'>
+{p.imageUrl}
+{p.id}
+{p.modelId}
+{p.createdAt}
+{p.updatedAt}
+{p.prompt}
+
+</div> : null
+))}
 </div>
 </div>
 
