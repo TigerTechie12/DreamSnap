6b20a0de9c9c275a3e8804ef4ee60d5951115793
Shreyash <shreyashiitr22@gmail.com>
2025-12-17 22:24:57 +0530

    initialized portal in the packs page

--- frontend/vite-project/src/pages/mypacks.tsx
+++ frontend/vite-project/src/pages/mypacks.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { createPortal } from "react-dom"
 export function MyPacks(){
     interface Packs{
 id:string
@@ -18,14 +19,26 @@ const fetching=async()=>{
     setPacks(genpacks)
 } 
 },[])   
-
+const [showModal,setShowModal]=useState(false)
 
 return <div>
-   <div className="flex"><h1>Photo Packs</h1> <button className="bg-blue-500"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-indigo-500">
+   <div className="flex"><h1>Photo Packs</h1> <button onClick={()=>{setShowModal(true)}} className="bg-blue-500"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-indigo-500">
 
   <path fillRule="evenodd" d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813a3.75 3.75 0 002.576-2.576l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.394a.75.75 0 010 1.422l-1.183.394c-.447.15-.799.502-.948.948l-.394 1.183a.75.75 0 01-1.422 0l-.394-1.183a1.5 1.5 0 00-.948-.948l-1.183-.394a.75.75 0 010-1.422l1.183-.394c.447-.15.799-.502.948-.948l.394-1.183A.75.75 0 0116.5 15z" clipRule="evenodd" />
 </svg> Generate Pack</button> </div>
- 
+{showModal && createPortal(<div><h2>Create New Pack</h2>
+<h3>Define your pack details and add multiple prompts. Each prompt will generate variations based on the total images specified.</h3>
+<div className="flex flex-col">
+    <h2>Pack Name</h2>
+    <input type="text" className="border-r-2" placeholder="e.g.,Valentine's Collection,Beach Vibes" />
+<h2>Pack Type</h2>
+ <input type="text" className="border-r-2" placeholder="e.g.,Valentine's,Beach,Royal,Adventure" />
+
+</div>
+</div>
+
+)}
+
     <h3>Create and manage themed image collections with custom prompts</h3>
 <h2 className="flex">Generating 
 <div role="status">
