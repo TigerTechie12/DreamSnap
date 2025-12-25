46514ac98320d2745e6c7dc166bdc5e5cda766b6
Shreyash <shreyashiitr22@gmail.com>
2025-12-25 13:24:37 +0530

    ui changes

--- frontend/vite-project/src/components/navbar.tsx
+++ frontend/vite-project/src/components/navbar.tsx
@@ -1,14 +1,19 @@
 import { CameraIcon } from '@heroicons/react/24/solid'
+import { useNavigate } from 'react-router-dom'
 export function Navbar() {
+  const navigate=useNavigate()
     return(
      <div className='bg-black'>
-    <div className="flex">
-    <button className="p-3 bg-blue-500 hover:bg-blue-600 rounded-full text-white transition-colors">
+    <div className="flex justify-between border border-gray-800 rounded-xl">
+   <div className='flex ml-6 mb-0'> <button  className="p-3 bg-blue-500 hover:bg-blue-600 rounded-full text-white transition-colors">
   <CameraIcon className="w-6 h-6" />
 </button>
-    <div>DreamSnap</div>
-    <button className='hover:bg-purple-500 rounded-2xl'>Sign in</button>
-    <button className='bg-blue-500 p-3 font-black rounded-2xl'>Get Started</button>
+ <div className='text-white font-bold text-3xl ml-4 mt-3'>DreamSnap</div>
+</div>
+   
+   <div> <button onClick={()=>{navigate('/signin')}} className='hover:bg-purple-500 bg-blue-500 p-3 m-2 rounded-2xl'>Sign in</button>
+    <button onClick={()=>{navigate('/signup')}} className='bg-blue-500 p-3 mr-6 hover:bg-purple-300 rounded-2xl'>Get Started</button></div>
+   
          </div>
       </div>
     )
